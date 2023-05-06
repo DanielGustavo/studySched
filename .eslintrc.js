9347b2a8a5afdd32c0dfbace2543c2c8ff59c320
DanielGustavo <dg520589@gmail.com>
2023-05-06 15:12:17 -0300

    build: setup eslint, prettier and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+};
